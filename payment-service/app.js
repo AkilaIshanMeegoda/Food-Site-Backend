2af5b74f92f688d3f548140e24c31fc6524841dd
Savishka Dilshan <savishkadilshanofficial@gmail.com>
2025-04-03 11:18:46 +0530

    Enhance MongoDB connection handling with error logging and specify database name

--- payment-service/app.js
+++ payment-service/app.js
@@ -5,11 +5,19 @@ import bodyParser from 'body-parser';
 import 'dotenv/config';
 
 const app = express();
+const dbName = "PaymentDB";
 
 const PORT = process.env.PORT || 5004;
 const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
 
-mongoose.connect(MONGODB_CONNECTION_STRING);
+try {
+    mongoose.connect(MONGODB_CONNECTION_STRING, {
+        dbName: dbName,
+    });
+    console.log(`Connected to MongoDB ${dbName} database.`);
+} catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+}
 
 app.use(bodyParser.json());
 app.use(cors({ origin: "*" }));
