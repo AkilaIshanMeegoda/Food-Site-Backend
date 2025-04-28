3ccba3c4b4e96ac747fcd34580b5a4d24d798277
chala16 <it22338952@my.sliit.lk>
2025-04-28 21:52:59 +0530

    add comments for routes

--- restaurant-service/routes/publicRoutes.js
+++ restaurant-service/routes/publicRoutes.js
@@ -2,18 +2,18 @@ const express = require("express");
 const router = express.Router();
 const publicController = require('../controllers/publicController');
 
-
+// Public routes for get all available restaurants
 router.get("/restaurants", publicController.getRestaurants);
-
+// Public route to get all available menu items for a restaurant
 router.get("/restaurants/:id/menu-items", publicController.getMenuItemsByRestaurant);
-
+//Public route to get a restaurant by ID
 router.get("/restaurants/:id", publicController.getRestaurantById);
-
+// Public route to get a available menu item
 router.get("/menu-items/:id", publicController.viewItemDetail);
-
+// Public route to get all available menu items for a restaurant by category /
 router.get("/restaurants/:id/menu-items/category/:category", publicController.getMenuItemsByCategory);
-
+// Public route to get all available menu items by category
 router.get("/menu-items/category/:category", publicController.getAllMenuItemsByCategory);
-
+// Public route to get all available menu items
 router.get('/all-menu-items', publicController.getAllAvailableMenuItems);
 module.exports = router;
